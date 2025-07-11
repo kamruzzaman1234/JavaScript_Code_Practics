1bfafb0b63a0e7b06b4a80e4e12feaa1500dbddf
Kamruzzaman <kamruzzamancse22@gmail.com>
2025-07-11 10:52:01 +0800

    added filter code

--- filter.js
+++ filter.js
@@ -3,5 +3,33 @@
 const number = [2,5,6,7,8,9,10]
 
 const evenNumber = number.filter(num=> num % 2 === 0)
-
 console.log("Your number is",evenNumber)
+
+// small people ber korar program 
+const people = [
+    {name:"Abdul", age:12},
+    {name:"kamrul", age:11},
+    {name:"khadija", age:29}
+]
+
+const smallPeople = people.filter(small=> small.age <= 18);
+console.log("You are very small and your age", smallPeople)
+
+// Fail kora people ber korar code 
+
+const people1 = [
+    {name:"Ali", marks:34},
+    {name:"Imam", marks:32},
+    {name:"Faysal", marks: 84},
+    {name:"labib", marks: 87},
+    {name:"Sajib", marks:21 }
+]
+
+const failPeople = people1.filter(mark=> mark.marks <= 33);
+console.log(`Your Fail students ${JSON.stringify(failPeople)}`)
+
+// Sudhu string ber korar program ber korar code .
+
+const mixedType = [1,"Hello", true, "String"]
+const typeString = mixedType.filter(item=> typeof item === "string")
+console.log(`Your String value is ${typeString}`)
